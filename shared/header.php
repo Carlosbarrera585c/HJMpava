31d75a5a0c06d9c8599cb5603b5fcf1b4ef5a873
Carlos <Carlos@DESKTOP-A5A4IK3>
2020-04-13 20:14:29 -0500

    Cambio de Copyright.

--- shared/header.php
+++ shared/header.php
@@ -1,51 +1,51 @@
-<header class="main-header">
+<!--<header class="main-header">-->
 
-        <!-- Logo -->
-        <a href="index2.html" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>AF</b></span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Activos Fijos</b></span>
-        </a>
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>AF</b></span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Activos Fijos</b></span>
+    </a>
 
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Navegación</span>
-          </a>
-          <!-- Navbar Right Menu -->
-          <div class="navbar-custom-menu">
+        </a>
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-              <!-- Messages: style can be found in dropdown.less-->
-              
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="assets/img/muser2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Area de Tecnología</span>
-            </a>
-            <ul class="dropdown-menu">
-              <!-- User image -->
-              <li class="user-header">
-                <img src="assets/img/muser2-160x160.jpg" class="img-circle" alt="User Image">
+                <!-- Messages: style can be found in dropdown.less-->
 
-                <p>
-                 Area de Tecnología
-                  <small>Marzo 27-23-2020</small>
-                </p>
-              </li>
-              <!-- Menu Footer-->
-              <li class="user-footer">
-                <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Salir</a>
-                </div>
-              </li>
-            </ul>
-          </li>
-              
-            </ul>
-          </div>
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="assets/img/muser2-160x160.jpg" class="user-image" alt="User Image">
+                        <span class="hidden-xs">Area de Tecnología</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <img src="assets/img/muser2-160x160.jpg" class="img-circle" alt="User Image">
 
-        </nav>
-      </header>
+                            <p>
+                                Area de Tecnología
+                                <small>Marzo 27-23-2020</small>
+                            </p>
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-right">
+                                <a href="#" class="btn btn-default btn-flat">Salir</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+
+    </nav>
+</header>
