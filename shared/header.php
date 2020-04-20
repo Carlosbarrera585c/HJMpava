861f310269636632c63e265c8361682d2249fae2
sistemas <sistemas@sistemas>
2020-04-20 09:03:47 -0500

    Cambio icono Tecnologia.

--- shared/header.php
+++ shared/header.php
@@ -22,13 +22,13 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="assets/img/muser2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="assets/img/Tecnoforce.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs">Area de Tecnología</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="assets/img/muser2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="assets/img/Tecnoforce.jpg" class="img-circle" alt="User Image">
 
                 <p>
                  Area de Tecnología
